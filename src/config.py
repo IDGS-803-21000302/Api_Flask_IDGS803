c95d6c07e09faac3459508450bcd4fcf6071ecc5
IDGS-803-21000302 <21000302@alumnos.utleon.edu.mx>
2024-03-01 18:49:04 -0600

    GET 01/03/2024 HTS

---
+++ src/config.py
@@ -0,0 +1,10 @@
+class DevelopmenConfig():
+    DEBUG= True
+    MYSQL_HOST='localhost'
+    MYSQL_USER='root'
+    MYSQL_PASSWORD='10012003'
+    MYSQL_DB='api_utl_idgs803'
+
+config={
+    'developmen':DevelopmenConfig
+}
